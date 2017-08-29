fe7882bf6279fd4da2b67e163970913671d647f5
jawad <jawad.akram347@gmail.com>
2017-08-30 01:54:47 +0500

    get request to get all cmapaigns

---
+++ src/app/components/GoogleCampaign/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+	Table,
+	TableBody,
+	TableHeader,
+	TableHeaderColumn,
+	TableRow,
+	TableRowColumn,
+} from 'material-ui/Table';
+import axios from 'axios';
+import SkyLight from 'react-skylight';
+import Autocomplete from 'react-google-autocomplete';
+
+export default class CampaignList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		}
+		this.fetchCampaigns()
+	}
+
+	fetchCampaigns = () => {
+	  axios.defaults.headers.common['Authorization'] = "Token " + localStorage.getItem('key');
+	  axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+	  axios.get('http://www.carderockllc.com/api/v1/google/campaigns/?token={2}').then(result => {
+	   console.log(data)
+
+	   console.log(result.data)
+	  }).catch(err => {
+   console.log(err)
+  })
+ }
+
+	render() {
+
+		return (
+			<div>
+    {/* Content here */}
+			</div>
+		);
+	}
+}
