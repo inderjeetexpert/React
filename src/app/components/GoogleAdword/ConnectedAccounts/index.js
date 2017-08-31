3619fc44c6e7b2c9804107bd15cbf21687a096b9
jawad <jawad.akram347@gmail.com>
2017-08-31 09:37:46 +0500

    connected google account List

--- src/app/components/GoogleAdword/ConnectedAccounts/index.js
+++ src/app/components/GoogleAdword/ConnectedAccounts/index.js
@@ -6,25 +6,20 @@ import {
 	TableHeaderColumn,
 	TableRow,
 	TableRowColumn,
-} from 'material-ui/Table';
-import axios from 'axios';
-import SkyLight from 'react-skylight';
+} from 'material-ui/Table'
+import axios from 'axios'
 import MoreDetailSection from '../../MoreDetail'
-import Autocomplete from 'react-google-autocomplete';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default class ConnectedAccountList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			busy: false,
 			recordcount: 0,
 			data: [],
 			errorMsg: null,
 		}
 		this.fetchCampaigns = this.fetchCampaigns.bind(this)
-		this.handleSearch = this.handleSearch.bind(this)
-		this.handleClearForm = this.handleClearForm.bind(this);
 		this.fetchCampaigns()
 	}
 
@@ -36,50 +31,17 @@ export default class ConnectedAccountList extends React.Component {
 		this.setState({ item: event.target.value })
 	}
 
-
-
-	handleClearForm(event) {
-		event.preventDefault();
-		this.setState({
-			errorMsg: null,
-			errorItem: null,
-			errorLoc: null,
-			item: '',
-			location: '',
-		});
-
-	}
-
-	handleSearch(event) {
-		event.preventDefault();
-		const query= this.state.item ;
-		if (!query) {
-			this.setState({ errorItem: 'Please provide business name!!', busy: false })
-			return;
-		}
-		this.setState({
-			data:[],
-			next:'http://www.carderockllc.com/api/v1/company/companies/?query='+query,
-			busy: true,
-			errorItem: null
-		})
-		let self=this
-		setTimeout(function () {
-			self.fetchCampaigns()
-		}, 500);
-
-	}
-
 	fetchCampaigns() {
-		let url = this.state.next
-		if(!url){
-			return;
-		}
-	  this.setState({ loadingCampaigns: true })
+	  this.setState({ loadingConnectedAccounts: true })
 	  axios.defaults.headers.common['Authorization'] = "Token " + localStorage.getItem('key');
 	  axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
-	  axios.get('http://www.carderockllc.com/api/v1/google/campaigns/?token={2}').then(result => {
-	   this.setState({ loadingCampaigns: false })
+	  axios.get('http://www.carderockllc.com/api/v1/google/campaigns/?token=EAAbqZCkO2k6gBAOOupYIlFHumfsBiXSGbkZAkbWkadWZAI1nwZCyuZCS3Y1lFmvDZB0hViAstho4eFDIMMeZCGNwfsQtPRIp1lrZAqaZCvVU3xQDZCDEot2twOZAgLuvPAvZCQMRoE7OM8SEOOmh59WHs3WMqEpHESObhmYbEJVQ9lJ8ZBJxH6qzfCc5eK5pxYVGE2tjjKnkYjJIinwZDZD').then(result => {
+			console.log('result')
+			console.log('\n')
+			console.log('\n')
+			console.log('\n')
+			console.log(result)
+	   this.setState({ loadingConnectedAccounts: false })
 		 let data = this.state.data
 		 result.data.results.map(d=>{
 			 data.push(d)
@@ -88,89 +50,33 @@ export default class ConnectedAccountList extends React.Component {
 
 
 	   this.setState({
-			 next : result.data.next,
 	    data: data,
-	    recordcount: result.data.length,
-	    busy: false,
-	    searchedName: this.state.item,
-	    searchedLocation: this.state.location
+	    recordcount: result.data.length
 	   })
 	   console.log(result.data)
 	  }).catch(err => {
-	   this.setState({ loadingCampaigns: false })
-	   this.setState({ busy: false });
+	   this.setState({ loadingConnectedAccounts: false })
    //console.log(err)
   })
  }
 
 	render() {
-		let loadingCampaigns = null;
-		if(this.state.loadingCampaigns){
-			loadingCampaigns = (
+		let loadingConnectedAccounts = null;
+		if(this.state.loadingConnectedAccounts){
+			loadingConnectedAccounts = (
 				<p style={{textAlign:'center'}}>
 						<i className="ion-arrow-shrink"></i>
 				</p>
 			)
 		}
-		let searchButton = null;
 		let errorMsg = null;
-		let errorItem = null;
-		let errorLoc = null;
-		let { item, location, recordcount, datailInfo, searchedName, searchedLocation } = this.state;
-		if (!this.state.busy) {
-			searchButton = <button type="submit" onClick={(event) => this.handleSearch(event)} className="btn btn-primary btn-lg">Search</button>;
-		} else {
-			searchButton = <button type="button" className="btn btn-primary btn-lg disabled" disabled><i className="fa fa-fw fa-spin fa-spinner"></i>Searching .. </button>
-		}
-
-
-
-		if (!this.state.errorItem) {
-			errorItem = null;
-		} else {
-			errorItem = <div className="input-group-addon"><i className="ion-alert-circled" style={{ fontSize: 20, color: '#ff0000', float: 'right', marginRight: -18, marginTop: -24 }}></i></div>
-		}
-
 
 		return (
 			<div>
-					{loadingCampaigns}
-					<div className="container-fluid">
-							<div className="search-header">
-									<div className="row">
-											<form onSubmit={(event) => this.handleSearch(event)}>
-													{errorMsg}
-													<div className="col-lg-9 col-md-8 col-sm-8 col-xs-12">
-															<div className="col-lg-7 col-md-7 col-sm-7 col-xs-12">
-																	<div className="form-group">
-																			{/* <label className="control-label" htmlFor="focusedInput">Search Item</label> */}
-																			<span className="input-group-addon"><i className="ion-android-search"></i></span>
-																			<input className="form-control" type="text" value={item} placeholder="Search for Campaigns" onChange={(event) => this.handItemChange(event)} />
-																			{errorItem}
-																	</div>
-															</div>
-
-													</div>
-													<div className="col-auto pull-right">
-															<button onClick={(event) => this.handleClearForm(event)} className="clear-button">Clear</button>
-															{searchButton}
-
-													</div>
-											</form>
-									</div>
-							</div>
-					</div>
+					{loadingConnectedAccounts}
 					<section className="">
 							<div className="container-fluid">
 									<div className="row">
-											<div className="col-md-12">
-
-													{/*<h5 className="record-count">
-															{searchedName && searchedLocation ?
-															<p>We found<span> {recordcount}</span> results for <span>{searchedName}</span> in <span>{searchedLocation}</span></p>
-															: 'Please provide item and location to search!'}
-													</h5>*/}
-											</div>
 											<div>
 													<div className=''>
 															<div className="body-border">
